40d241ddfccfde52b3442a8744e204bc8e2f141d
murthy-koppu <murthy.koppu@imaginea.com>
2013-09-23 09:30:54 +0530

    Added To MySQLLocator and read version

--- src/com/imaginea/serverlocator/impl/MySQLLocator.java
+++ src/com/imaginea/serverlocator/impl/MySQLLocator.java
@@ -1,26 +1,67 @@
 package com.imaginea.serverlocator.impl;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.Socket;
 
 import com.imaginea.serverlocator.util.Utils;
 
 public class MySQLLocator implements ServerLocator{
 
+	int packetPosition = -1;
+	int packetLen = -1;
+	private int readPktPos() throws Exception{
+		if(packetPosition >= packetLen)
+			throw new Exception();
+		return packetPosition++;
+	}
+	
 	@Override
 	public ServerProperties parseToServerProp(InetAddress iNetAddr, int port) {
 			try {
-				Utils.getDataInStreamFromServer(iNetAddr, port);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		
-		
+				DataInputStream dInStream = Utils.getDataInStreamFromServer(iNetAddr, port);
+				byte[] packet = getPacketLength(dInStream);
+				if(packet == null)
+					return null;
+				// First Byte of packet refers to protocol version
+				if(packet[readPktPos()] == -1)
+					return null;			
+				
+				String version = readString(packet, readPktPos());										
+				
+			} catch (Exception e) {
+				return null;
+			}	
 		
 		return null;
+	}
+	
+	private byte[] getPacketLength(DataInputStream dInStream) throws IOException {
+		byte[] packetLengthInBytes = new byte[4];
+		int noOfLengthBytes = dInStream.read(packetLengthInBytes, 0, 3);				
+		if(noOfLengthBytes < 4)
+			return null;
+		int packetLength = (packetLengthInBytes[0] & 0xff) +
+		((packetLengthInBytes[1] & 0xff) << 8) +
+		((packetLengthInBytes[2] & 0xff) << 16);
+		this.packetLen = packetLength;
+		byte[] packet = new byte[packetLength+1];
+		int noOfPacktetBytes = dInStream.read(packet, 0, packetLength);
+		if(noOfPacktetBytes != packetLength)
+			return null;
+		packet[packetLength] = 0;
+		this.packetPosition = 0;
+		return packet;
+	}
+	
+	
+	private String readString(byte[] packet, int off) throws Exception{
+		String data = "";
+		byte byteData;
+		while((byteData = packet[readPktPos()]) != 0)
+			data += byteData;
+		
+		return data;
 	}
 	
 }
--- src/com/imaginea/serverlocator/util/Utils.java
+++ src/com/imaginea/serverlocator/util/Utils.java
@@ -1,10 +1,12 @@
 package com.imaginea.serverlocator.util;
 
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+
 
 public class Utils {
 
@@ -23,5 +25,16 @@ public class Utils {
 		Socket clientSkt = new Socket(iNetAddr,port);
 		return getDataInStreamFromServer(clientSkt);
 	}
+	
+	 private final int readFully(InputStream in, byte[] b, int off, int len)
+     throws IOException {
+     if (len < 0) {
+         throw new IndexOutOfBoundsException();
+     }
+
+     int n = 0;
+     int count = in.read(b, off + n, len - n);
+     return n;
+ }
 	
 }
